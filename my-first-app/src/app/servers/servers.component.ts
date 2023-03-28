4f74976a21be178c443c863cb21f81b89dde8bb9
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2023-03-28 12:00:54 -0300

    binding properties and events

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -4,9 +4,23 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]',
 	// selector: '.app-servers',
 	selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+	allowNewServer = false;
+	serverCreationStatus = 'No server was created!';
 
+	constructor(){
+		setTimeout(() => {
+			this.allowNewServer = true;
+		}, 2000)
+
+	}
+
+	onCreateServer(){
+		this.serverCreationStatus = 'Server was created!';
+	}
+
+	ngOnInit() {}
 }
