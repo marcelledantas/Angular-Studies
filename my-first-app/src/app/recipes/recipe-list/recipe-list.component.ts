ee352504675f62dc1d25d731553d9df18288f056
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2023-05-17 15:20:23 -0300

    adding recipe items component

--- my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit{
 	
+	recipes: Recipe[] = [
+		new Recipe('A Test Recipe', 'This is a simple test recipe', 'https://www.acouplecooks.com/wp-content/uploads/2021/03/Cheese-Tortellini-011.jpg')
+	];
+
 	ngOnInit(): void {
 		throw new Error('Method not implemented.');
 	}
