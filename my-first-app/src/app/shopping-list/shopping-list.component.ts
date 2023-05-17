3b50540b5e23ae3f0c4e28b3a0b3b052b12f2219
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2023-05-17 19:12:51 -0300

    adding shopping list

--- my-first-app/src/app/shopping-list/shopping-list.component.ts
+++ my-first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+	ingredients: Ingredient[] = [
+		new Ingredient('Apples', 5),
+		new Ingredient('Tomatos', 2)
+	];
 
+	constructor(){}
+
+	ngOnInit() {}
 }
